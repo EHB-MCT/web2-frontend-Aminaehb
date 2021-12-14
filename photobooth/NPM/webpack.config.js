eabebef9b10b7ceabd56520a3bf2c02231cbeed6
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-12-14 11:34:26 +0100

    webpack code added

--- photobooth/NPM/webpack.config.js
+++ photobooth/NPM/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: 'main.js',
-  },
-  mode: 'production',
-  watch: true
+    entry: './photobooth/NPM/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'main.js',
+    },
+    mode: 'production',
+    watch: true
 };
